f02dec83b3103746623917fb0e4c50c967303366
usmaniraghib <javaprogrammers786@gmail.com>
2023-08-23 21:44:06 +0530

    TODO

--- src/com/raghib/javascriptexecutor/ScrollHorizontal.java
+++ src/com/raghib/javascriptexecutor/ScrollHorizontal.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ScrollHorizontal {
-
+	// TODO Program Pending to implement.
 	public static String url = "https://www.album.alexflueras.ro/";
 
 	public static void main(String[] args) throws InterruptedException {
