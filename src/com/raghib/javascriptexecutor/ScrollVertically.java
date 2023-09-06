46107d742e4cea01a9384f1ab95881e1d5289a7f
usmaniraghib <javaprogrammers786@gmail.com>
2023-09-06 16:43:34 +0530

    Converted into maven project.

--- src/com/raghib/javascriptexecutor/ScrollVertically.java
+++ src/com/raghib/javascriptexecutor/ScrollVertically.java
@@ -1,29 +1,43 @@
 package com.raghib.javascriptexecutor;
 
+/**
+ * Reference:-
+ * https://www.youtube.com/watch?v=McX3fS5Q_Yc
+ * 
+ * Previously (Selenium-3) - JavaScriptExecutor -> for scroll.
+ * 
+ * Now (Selenium-4.2) - Action Class -> scrollByAmount()
+ */
+
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ScrollVertically {
-	
+import com.base.BaseClass;
+
+public class ScrollVertically extends BaseClass {
+
+	public static WebDriver driver;
+	public static String browserName = "chrome";
+	public static String browserVersion = "116";
 	public static String url = "https://www.amazon.in/";
-	
+
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		// Chrome Browser
+		driver = BaseClass.getDriver(browserName, browserVersion);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.get(url);
-		
-		JavascriptExecutor js = (JavascriptExecutor)driver;
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scroll(0,4500)");
-		Thread.sleep(5000);
 		
-		driver.findElement(By.cssSelector("#navBackToTop")).click();
 		Thread.sleep(5000);
-		driver.quit();
+		driver.findElement(By.xpath("//div[@class='nav-logo-base nav-sprite']")).click();
+		Thread.sleep(5000);
+
+		BaseClass.quitDriver();
 	}
 }
